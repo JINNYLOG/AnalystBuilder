8eaa1449111ac824fded692b5d85964dec73d35e
JINNYLOG <149363095+JINNYLOG@users.noreply.github.com>
2024-10-23 09:02:45 +0900

    Create TMI (Too Much Information).sql

---
+++ MySQL/TMI (Too Much Information).sql
@@ -0,0 +1,12 @@
+# Often when you're working with customer information you'll want to sell that data to a third party. 
+
+# Sometimes it is illegal to give away sensitive information such as a full name.
+
+# Here you are given a table that contains a customer ID and their full name.
+
+# Return the customer ID with only the first name of each customer.
+
+SELECT customer_id, 
+       TRIM(SUBSTRING_INDEX(Full_Name, ' ', 1)) AS First_name 
+  FROM customers 
+;
